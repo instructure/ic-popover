a1ce7cb68d4475cf599ada121d8e61262ac277a4
Ryan Florence <rpflorence@gmail.com>
2013-09-18 08:43:43 -0600

    get rid of deprecation warnings

--- example/app/main.js
+++ example/app/main.js
@@ -1,10 +1,16 @@
 import Ember from 'ember';
 import templates from './app-templates';
+var App = window.App = Ember.Application.create();
+App.ApplicationController = Ember.Controller.extend({
+  actions: {
+    toggleShowPopover: function() {
+      this.toggleProperty('showPopover');
+    }
+  }
+});
 
 import { IcPopoverComponent } from 'ic-popover';
 import addonTemplates from 'ic-popover/templates';
-
-var App = Ember.Application.create();
 App.IcPopoverComponent = IcPopoverComponent;
 
 export default App;
